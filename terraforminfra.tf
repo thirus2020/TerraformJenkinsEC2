d439c1ab2a847e49f33464eb81516b7ece73d8af
thiru-user <thiru-user@domain.com>
2026-09-17 18:48:22 +0530

    modified file

--- terraforminfra.tf
+++ terraforminfra.tf
@@ -74,6 +74,7 @@ resource "aws_nat_gateway" "VPC-01-NAT-GW"{
 resource "aws_subnet" "VPC-01-PrivateSN"{
     vpc_id = aws_vpc.VPC-01.id
     cidr_block = "10.0.2.0/24"
+    
     tags={
         Name="VPC-01-PrivateSN"
     }
